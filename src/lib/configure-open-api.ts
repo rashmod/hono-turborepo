1254d5b1af5f3c0297bff7d50efa1b9cffb3faf9
Bhushan <alhatbhushan@gmail.com>
2025-01-26 11:53:48 +0530

    added scalar for api documentation

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,3 +1,5 @@
+import { apiReference } from '@scalar/hono-api-reference';
+
 import type { AppOpenApi } from '@/lib/types';
 
 import packageJson from '../../package.json';
@@ -10,4 +12,19 @@ export function configureOpenAPI(app: AppOpenApi) {
 			version: packageJson.version,
 		},
 	});
+
+	app.get(
+		'/reference',
+		apiReference({
+			spec: {
+				url: '/doc',
+			},
+			theme: 'kepler',
+			layout: 'classic',
+			defaultHttpClient: {
+				targetKey: 'js',
+				clientKey: 'fetch',
+			},
+		})
+	);
 }
