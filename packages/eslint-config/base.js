52fbb91e3b57a4ae3fcb9728f03d0c036ec24b70
Bhushan <alhatbhushan@gmail.com>
2025-01-30 12:38:06 +0530

    added eslint-plugin-only-warn

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,10 +1,12 @@
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import 'eslint-plugin-only-warn';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{ ignores: ['dist/**'] },
+
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
-	{ ignores: ['dist/**'] },
 ];
